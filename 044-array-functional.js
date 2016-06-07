f978a7770d9eb44868643e755f60b566d98e514f
Udi Oron <udioron@gmail.com>
2016-06-07 15:48:49 +0300

    added array.map, array.some etc.

--- 044-array-functional.js
+++ 044-array-functional.js
@@ -1,1 +1,32 @@
 // Array methods - functional
+
+var arr = [
+    'banana',
+    'apple',
+    'melon',
+    'pear',
+    'orange'
+];
+
+var arr2 = arr.map(function (x) {
+    return x.toUpperCase();
+});
+
+console.log(arr2);
+
+var arr3 = arr.map(function (x) {
+    return x.charAt(0) == "a";
+});
+
+console.log(arr3);
+
+var f = function (x) {
+    return x.charAt(0) == "a";
+};
+
+var arr4 = arr.map(f);
+console.log(arr4);
+
+console.log(arr.every(f));
+console.log(arr.some(f));
+console.log(arr.filter(f));
